1dc43cb27140216733e3088a9391eb0bebce0cfb
scian0204 <scian0204@gmail.com>
2023-04-19 23:22:54 +0900

    fix error
    게시글 상세 정보 불러오기 오류 수정

--- src/pages/modals/Post.jsx
+++ src/pages/modals/Post.jsx
@@ -7,7 +7,7 @@ import { useParams } from "react-router-dom";
 
 function Post(props) {
   const [isEditPostOpen, setEditPostOpen] = useState(false);
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
 
   Axios.defaults.withCredentials = true; //axios 사용 컴포넌트 마다 한번씩 붙여넣을 것
 
@@ -27,8 +27,7 @@ function Post(props) {
       if(res.data == null) {
         alert("생성되지 않은 개시물 입니다.");        
       }
-      setData([res.data, ...data]);
-      console.log(res.data.board);
+      setData(res.data);
     })
     .catch(err => {
       alert("에라가 발생했습니다.");
@@ -38,42 +37,33 @@ function Post(props) {
 
   let urId = localStorage.getItem('userId');
 
-  if(!data) {
+  if(data == null) {
     return <div className="Loding">로딩 중...</div>
   } else {
-    if(urId === data.userId) {
-      return (
-        <div className="modelbox">
-          <div className="modelContent">
-            <button className="closeModel" onClick={handleClose}>
-              X
-            </button>
-            <div className="contents">
-              <div className="title"><h3>{data.board}</h3></div>
-            </div>
-          </div>
+    return (
+      <div className="modelbox">
+      <div className="modelContent">
+        <button className="closeModel" onClick={handleClose}>
+          X
+        </button>
+        <div className="contents">
+          <div className="title"><h3>{data.board.title}</h3> 조회수 : {data.board.views}</div>
+          <p className="tags">{data.tags.map((tag)=><span key={tag.tagId}>| {tag.tagName} |</span>)}</p>
+          <p className="content">{data.board.content}</p>
+          <p>파티 아이디 : {data.party.partyId}</p>
+          <p>{data.party.current} / {data.party.total}</p>
         </div>
-      );
-    } else {
-      return (
-        <div className="modelbox">
-        <div className="modelContent">
-          <button className="closeModel" onClick={handleClose}>
-            X
-          </button>
-          <div className="contents">
-            <div className="title"><h3>{data.board}</h3></div>
-          </div>
+        { urId === data.board.userId &&
           <div className="btn_wrapper">
             <MyButton onClick={onEditPostClick} text={"수정하기"} />
           </div>
-        </div>
-        {isEditPostOpen && (
-          <EditPost setEditPostOpen={setEditPostOpen} />
-        )}
+        }
       </div>
-      )
-    }
+      {isEditPostOpen && (
+        <EditPost setEditPostOpen={setEditPostOpen} />
+      )}
+    </div>
+    )
   }
 }
 
